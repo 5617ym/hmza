5659778645e40f3eb6be01a1e12f47ceca8b8acb
yousef mohammed <5617ym@gmail.com>
2026-03-12 21:07:06 +0300

    Update sector-profiles.js

--- api/_lib/sector-profiles.js
+++ api/_lib/sector-profiles.js
@@ -1,1 +1,12 @@
 
+const bank = require("./profiles/bank-profile");
+const insurance = require("./profiles/insurance-profile");
+const operating = require("./profiles/operating-profile");
+const reit = require("./profiles/reit-profile");
+
+module.exports = {
+  bank,
+  insurance,
+  operating_company: operating,
+  reit
+};
