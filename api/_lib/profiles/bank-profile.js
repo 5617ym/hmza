78929a27631019b795ed732ae486ea4773553e97
yousef mohammed <5617ym@gmail.com>
2026-03-13 19:28:12 +0300

    Update bank-profile.js

--- api/_lib/profiles/bank-profile.js
+++ api/_lib/profiles/bank-profile.js
@@ -1,65 +1,57 @@
 module.exports = {
   sector: "bank",
 
-  // --------------------------------
-  // Income Statement Structure
-  // --------------------------------
-  incomeStatement: [
-    "special commission income",
-    "special commission expense",
-    "net special commission income",
-    "fee income",
-    "trading income",
-    "investment income",
-    "total operating income",
-    "operating income",
-    "net income",
-    "profit for the year",
-    "صافي الدخل",
-    "دخل العمولات الخاصة",
-    "صافي دخل العمولات الخاصة"
-  ],
+  statements: {
 
-  // --------------------------------
-  // Balance Sheet Structure
-  // --------------------------------
-  balanceSheet: [
-    "cash and balances with central banks",
-    "due from banks",
-    "financing and advances",
-    "loans and advances",
-    "investments",
-    "total assets",
-    "customer deposits",
-    "due to banks",
-    "total liabilities",
-    "share capital",
-    "retained earnings",
-    "total equity",
-    "ودائع العملاء",
-    "اجمالي الموجودات",
-    "اجمالي المطلوبات",
-    "حقوق الملكية"
-  ],
+    income: [
+      "special commission income",
+      "special commission expense",
+      "net special commission income",
+      "fee income",
+      "trading income",
+      "investment income",
+      "total operating income",
+      "operating income",
+      "net income",
+      "profit for the year",
+      "صافي الدخل",
+      "دخل العمولات الخاصة",
+      "صافي دخل العمولات الخاصة"
+    ],
 
-  // --------------------------------
-  // Cash Flow Structure
-  // --------------------------------
-  cashFlow: [
-    "net cash from operating activities",
-    "net cash used in investing activities",
-    "net cash from financing activities",
-    "cash and cash equivalents",
-    "increase in cash",
-    "decrease in cash",
-    "التدفقات النقدية من الانشطة التشغيلية",
-    "التدفقات النقدية من الانشطة الاستثمارية",
-    "التدفقات النقدية من الانشطة التمويلية"
-  ],
+    balance: [
+      "cash and balances with central banks",
+      "due from banks",
+      "financing and advances",
+      "loans and advances",
+      "investments",
+      "total assets",
+      "customer deposits",
+      "due to banks",
+      "total liabilities",
+      "share capital",
+      "retained earnings",
+      "total equity",
+      "ودائع العملاء",
+      "اجمالي الموجودات",
+      "اجمالي المطلوبات",
+      "حقوق الملكية"
+    ],
 
-  // --------------------------------
-  // Sector Keywords
-  // --------------------------------
+    cashflow: [
+      "net cash from operating activities",
+      "net cash used in investing activities",
+      "net cash from financing activities",
+      "cash and cash equivalents",
+      "increase in cash",
+      "decrease in cash",
+      "التدفقات النقدية من الانشطة التشغيلية",
+      "التدفقات النقدية من الانشطة الاستثمارية",
+      "التدفقات النقدية من الانشطة التمويلية"
+    ]
+
+  },
+
   keywords: [
     "bank",
     "banks",
