25ee530685afa412c9f5fe15f7891e675814cf6d
yousef mohammed <5617ym@gmail.com>
2026-03-13 19:25:38 +0300

    Update operating-profile.js

--- api/_lib/profiles/operating-profile.js
+++ api/_lib/profiles/operating-profile.js
@@ -1,8 +1,81 @@
-
 module.exports = {
   sector: "operating_company",
-  incomeStatement: [],
-  balanceSheet: [],
-  cashFlow: [],
-  keywords: []
+
+  statements: {
+    income: [
+      "revenue",
+      "sales",
+      "cost of sales",
+      "cost of revenue",
+      "gross profit",
+      "operating profit",
+      "operating income",
+      "net profit",
+      "net income",
+      "earnings per share",
+      "الايرادات",
+      "الإيرادات",
+      "المبيعات",
+      "تكلفة المبيعات",
+      "تكلفة الايرادات",
+      "مجمل الربح",
+      "الربح التشغيلي",
+      "صافي الربح"
+    ],
+
+    balance: [
+      "assets",
+      "liabilities",
+      "equity",
+      "total assets",
+      "total liabilities",
+      "total equity",
+      "current assets",
+      "non-current assets",
+      "current liabilities",
+      "non-current liabilities",
+      "الموجودات",
+      "الأصول",
+      "المطلوبات",
+      "الالتزامات",
+      "حقوق الملكية",
+      "إجمالي الموجودات",
+      "إجمالي المطلوبات",
+      "إجمالي حقوق الملكية"
+    ],
+
+    cashflow: [
+      "cash flows from operating activities",
+      "cash flows from investing activities",
+      "cash flows from financing activities",
+      "cash and cash equivalents",
+      "operating activities",
+      "investing activities",
+      "financing activities",
+      "التدفقات النقدية من الأنشطة التشغيلية",
+      "التدفقات النقدية من الأنشطة الاستثمارية",
+      "التدفقات النقدية من الأنشطة التمويلية",
+      "النقدية وما يعادلها"
+    ]
+  },
+
+  keywords: [
+    "revenue",
+    "sales",
+    "cost of sales",
+    "gross profit",
+    "operating profit",
+    "net income",
+    "inventory",
+    "trade receivables",
+    "trade payables",
+    "الايرادات",
+    "المبيعات",
+    "تكلفة المبيعات",
+    "مجمل الربح",
+    "الربح التشغيلي",
+    "المخزون",
+    "المدينون التجاريون",
+    "الموردون"
+  ]
 };
